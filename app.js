5e3fb484c2f0c053d154ab20b0d0963993acea15
AhmadTijani <ahmadtijani84@gmail.com>
2021-07-28 20:20:09 +0100

    new commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello'); 
+// string, Numbers, Boolean, Null, Undefined, Symbol
+
+const name ='John';
+const age = 30;
+const rating = 5.4;
+const isCool = true;
+const x =null;
+const y = undefined;
+let z;
+
+//concatenation
+console.log('My name is' +name+ 'and i am' +age); 
+//Template string
+console.log(`My name is ${name} and i am ${age}`);
+
+console.log('hello');
