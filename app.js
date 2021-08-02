e0c2501032f5efefc7c905cc232b46cd12441e9b
AhmadTijani <ahmadtijani84@gmail.com>
2021-08-02 17:53:34 +0100

    new commit

--- app.js
+++ app.js
@@ -45,8 +45,70 @@ fruits.pop();
 
 console.log(Array.isArray(fruits));
 
-console.log(fruits.indexOf('strawberries'))
+console.log(fruits.indexOf('strawberries'));
 
 console.log(fruits);
 
+const cars =['corrola', 'Honda', 'Mazda'];
 
+cars[3] ='Benz';
+
+console.log(cars);
+
+const person= {
+    firstName: 'John',
+    lastName: 'David',
+    age: '21',
+    hobbies: ['reading', 'dancing', 'football'],
+    address:{
+        street: '2 Ikoyi crescent',
+        city: 'Ikoyi',
+        state: 'Lagos, Nigeria',
+    }
+}
+
+/*console.log(person.lastName, person.firstName);
+console.log(person.address.city);*/
+
+/*const { firstName, lastName , address: {city}} = person;
+
+console.log(city);*/
+
+person.email = 'john@yahoo.com';
+
+console.log(person);
+
+const todos =[
+    {
+        id: 1,
+        text: 'Take ot trash',
+        isCompleted: true
+    },
+    {
+        id: 1,
+        text: 'Meeting with the boss',
+        isCompleted: false
+    },
+    {
+        id: 1,
+        text: 'Marketing our products',
+        isCompleted: true
+    },
+];
+
+/*console.log(todos[2].text);*/
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
+
+// For
+for(let i=0; i<=10; i++) {
+    console.log(`For Loop Number: ${i}`);
+}
+
+// While
+let i=0;
+while(i<10) {
+    console.log(`while loop Number: ${i}`);
+    i++;
+}
